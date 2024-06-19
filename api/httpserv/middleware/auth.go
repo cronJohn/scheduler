aa62ff47b1867f083a797f437c7b80ad0b211d82
cronJohn <51012201+cronJohn@users.noreply.github.com>
2024-06-19 10:14:12 -0500

    Update auth middleware to correctly use cookies

--- api/httpserv/middleware/auth.go
+++ api/httpserv/middleware/auth.go
@@ -1,11 +1,14 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 func Auth(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		cookie, err := r.Cookie("auth")
-		if err != nil || cookie.Value != "authenticated" {
+		if err != nil || cookie.Value != os.Getenv("SS_CK") {
 			http.Redirect(w, r, "/login", http.StatusSeeOther)
 			return
 		}
